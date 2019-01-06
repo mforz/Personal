bd80df07cad0242d944903fd8cc61eb8e0352b9d
mforz <irom@sina.cn>
2019-01-06 20:07:38 +0800

    zys

--- mo/src/pages/Movie/index.vue
+++ mo/src/pages/Movie/index.vue
@@ -2,12 +2,18 @@
     <div class="movie">
         <h3 style="text-align:center">-电影-</h3>
         <p style="text-align:center">
-            <a @click="release">热映</a>-<a>推荐</a>-<a @click="classic">经典</a>
+            <a :class="name==1?'active':''" @click="release">热映</a>-
+            <a :class="name==2?'active':''">推荐</a>-
+            <a :class="name==3?'active':''" @click="classic">经典</a>
         </p>
         <div class="release">
             <div v-for="(item ,i) in list" :key="i">
                 <div class="list">
-                    <img :src="item.images.medium" width="100" height="150" />
+                    <div>
+                        <a target="_block" :href="item.alt">
+                            <img :src="item.images.medium" width="100" height="150" />
+                        </a>
+                    </div>
                     <p class="item">
                         <span>{{`${item.title} (${item.year})`}}</span>
                         <span>评分：<i>{{item.rating?item.rating.average:''}}</i> </span>
@@ -17,6 +23,7 @@
                     </p>
                 </div>
             </div>
+            <Loading :show="!list.length" />
         </div>
 
         <div>
@@ -28,14 +35,19 @@
 
 <script>
 import Fetch from '@/static/Fetch/'
+import Loading from '@/components/Loading'
 let rand=[]
 export default {
     name:'Wallpaper',
+    components:{
+        Loading
+    },
     data(){
         return{
             list:[],
             height:0,
             i:0,
+            name:1,
         }
     },
     mounted() {
@@ -43,11 +55,13 @@ export default {
     },
     methods: {
         release(){
+             this.list=[],this.name=1
              Fetch('/db-movie-release?start='+this.i).then(res=>{
                 this.list=res.subjects
             }).catch()
         },
         classic(){
+            this.list=[],this.name=3
             Fetch('/movie250?start='+this.i).then(res=>{
                 this.list=res.subjects
             }).catch()
@@ -58,9 +72,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .movie{
     width:100%;
+    font-size:14px;
+}
+a{
+    text-decoration-color:#B22222;
+}
+.movie .active{
+    text-decoration:underline;
 }
 .movie .release{
     width:80%;
@@ -77,6 +98,7 @@ export default {
     padding-left:8px;
 
 }
+
 .item span{
     margin:0 5px;
 }
